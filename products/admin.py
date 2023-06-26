fc1cd19a583cfc3978e961de74a5204a0e10cb15
Carvu94 <cmatej08@gmail.com>
2023-06-26 17:05:08 +0100

    costumize admin for product and console

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 from .models import Product, Console
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Console)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'console',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+class ConsoleAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Console, ConsoleAdmin)
