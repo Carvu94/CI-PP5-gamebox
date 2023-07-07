ebc5fcce27becd68747a32f044f644e6242f27b8
Carvu94 <cmatej08@gmail.com>
2023-07-07 13:45:52 +0100

    add rest of messages in bag views

--- bag/views.py
+++ bag/views.py
@@ -16,14 +16,15 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the bag """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-        messages.success(request, f'Added {product.name} to your bag.')
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}.')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag.')
@@ -35,14 +36,18 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Addjust the quantity of the specified product to the bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}.')
     else:
         del bag[item_id]
+        messages.success(request, f'Removed {product.name} from your bag.')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -56,9 +61,11 @@ def remove_from_bag(request, item_id):
 
     try:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag.')
 
         request.session['bag'] = bag
 
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=200)
