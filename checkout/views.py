7eff09a8337c94639feca333c8861afb01d7c3f7
Carvu94 <cmatej08@gmail.com>
2023-07-09 22:38:22 +0100

    finalize bassic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -27,14 +27,16 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
-    print(intent)
-
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key missing.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51N8PAUCo9waNALwvMPP2KUyGhfcRCmjCeZlpGAq9Pf1jhP3POCXecri65iQ4rgBbTVrFY5gUNoF71dExaPAQbX0600XGzHdDgC',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
