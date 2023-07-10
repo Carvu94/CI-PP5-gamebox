b552d59437e43f6f7362ea8e7b735c75974c8a85
Carvu94 <cmatej08@gmail.com>
2023-07-10 15:33:44 +0100

    add checkout form data caching

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -42,7 +42,8 @@ def webhook(request):
     # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeded': handler.handle_payment_intent_succeded,
-        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed':
+        handler.handle_payment_intent_payment_failed,
     }
 
     # Get the webhook type from Stripe
