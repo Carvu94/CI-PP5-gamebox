cce06ee359b76dc6b4daa3a7cf45ef8e4fb0ebb3
Carvu94 <cmatej08@gmail.com>
2023-07-15 13:14:44 +0100

    add forms.py in products

---
+++ products/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Product, Console
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        consoles = Console.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in consoles]
+
+        self.fields['console'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
