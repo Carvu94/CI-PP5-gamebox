690ab6de15c5a6759349ab9230faee5e2ef72aa1
Carvu94 <cmatej08@gmail.com>
2023-07-21 01:52:34 +0100

    add 404.html and view

--- gamebox/urls.py
+++ gamebox/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import handler404
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -29,3 +31,5 @@ urlpatterns = [
     path('wishlist/', include('wishlist.urls')),
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'gamebox.views.handler404'
